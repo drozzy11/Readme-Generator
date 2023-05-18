0679d3559d6dcf1e4396009c24db8646e3783c98
drozzy11 <ruiz.diego071299@gmail.com>
2023-05-18 01:23:38 -0500

    Added all provided files to git hub

---
+++ Develop/index.js
@@ -0,0 +1,25 @@
+// TODO: Include packages needed for this application
+const markdown = require('../generateMarkdown')
+// TODO: Create an array of questions for user input
+const questions = [
+    'What is the title of your project?',
+    'Give a brief description of your project.',
+    'What are your instalation instructions?',
+    'Please provide any usage information.',
+    'Insert contribution guidelines.',
+    'What Licencses are being used?',
+    'Insert testing instructions.',
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    if (index.js) 
+    run(writeToFile)
+}
+
+// Function call to initialize app
+init();
